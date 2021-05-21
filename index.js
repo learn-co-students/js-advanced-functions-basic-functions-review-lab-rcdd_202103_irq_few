a950650ff723f4b7d67d8d8fe57d269ff4f0c98e
Hasar Ali <hasar.ali@protonmail.com>
2021-05-21 17:29:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+  return `This Saturday, I want to ${activity}!`
+}
+
+mondayWork = function (activity='go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(wrapper='*') {
+  return function (adj='special') {
+    return `You are ${wrapper}${adj}${wrapper}!`
+  }
+}
+
+let Calculator;
+Calculator = {
+  add: function(x, y){return x+y},
+  subtract: function(x,y){return x-y},
+  multiply: function(x,y){return x*y},
+  devide: function(x,y){return x/y},
+}
+
+function actionApplyer(start, fncs=[]) {
+  return !fncs ? start : fncs.forEach()
+  
+  if (!fncs.length) return start
+  
+  fncs.forEach(fnc => fnc(start))
+  
+  return start
+}
