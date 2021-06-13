977803b966a55e54f51ae31ed05b1dc82181d04d
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-06-13 12:56:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = function(style="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
+
+const Calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b;
+  }
+}
+
+const actionApplyer = function(start, ray) {
+  let a = start
+
+  for (let i of ray ){
+    a = i(a)
+  }
+
+  return a
+}
